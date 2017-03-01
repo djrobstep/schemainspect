d7381046587d793a5516e6bf1661f77ec55d890a
Robert Lechte <robert.lechte@digital.gov.au>
2017-03-02 10:27:03 +1100

    version bump

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='schemainspect',
-    version='0.1.1488167750',
+    version='0.1.1488410765',
     url='https://github.com/djrobstep/schemainspect',
     description='Schema inspection for PostgreSQL',
     long_description=io.open('README.rst').read(),
