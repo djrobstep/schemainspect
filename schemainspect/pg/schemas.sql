30b7313a55254a8914952c7a0c28c3f0d7714c82
djrobstep <djrobstep@users.noreply.github.com>
2018-07-24 21:21:01 +1000

    Tweak schema query (#12)
    * fix reqs
    
    * tweak schema inspection query
    
    * version bump
    
    * fix percentage sign encoding

--- schemainspect/pg/schemas.sql
+++ schemainspect/pg/schemas.sql
@@ -2,5 +2,5 @@ select
     nspname as schema
 from
     pg_catalog.pg_namespace
--- SKIP_INTERNAL where nspname not in ('pg_internal', 'pg_catalog', 'information_schema', 'pg_toast', 'pg_temp_1', 'pg_toast_temp_1')
+-- SKIP_INTERNAL where nspname not in ('pg_internal', 'pg_catalog', 'information_schema', 'pg_toast') and nspname not like 'pg_temp_%' and nspname not like 'pg_toast_temp_%'
 order by 1;
