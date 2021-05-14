e32ab287b8caeb17fa83e5afbb6ea60efbd5148c
mgbiotech <84165050+mgbiotech@users.noreply.github.com>
2021-05-14 19:02:06 +1000

    Also include SEQUENCE & COLUMN privileges

--- schemainspect/pg/sql/privileges.sql
+++ schemainspect/pg/sql/privileges.sql
@@ -1,19 +1,27 @@
 select
-    table_schema   as schema,
-    table_name     as name,
-    'table'        as object_type,
-    grantee        as "user",
-    privilege_type as privilege
+    n.nspname as schema, 
+    c.relname as name,
+    case
+        when c.relkind in ('r', 'v', 'm', 'f', 'p') then 'table'
+        when c.relkind = 'S' then 'sequence'
+        else null end as object_type, 
+    pg_get_userbyid(acl.grantee) as "user", 
+    acl.privilege
 from
-    information_schema.role_table_grants r
-    left join pg_tables t
-        on t.schemaname = r.table_schema and t.tablename = r.table_name
-    left join pg_views v
-        on v.schemaname = r.table_schema and v.viewname = r.table_name
+    pg_catalog.pg_class c
+    join pg_catalog.pg_namespace n
+         on n.oid = c.relnamespace,
+    lateral (select aclx.*, privilege_type as privilege
+             from aclexplode(c.relacl) aclx
+             union
+             select aclx.*, privilege_type || '(' || a.attname || ')' as privilege
+             from
+                 pg_catalog.pg_attribute a
+                 cross join aclexplode(a.attacl) aclx
+             where attrelid = c.oid and not attisdropped and attacl is not null ) acl
 where
-    r.grantee is distinct from t.tableowner
-    and r.grantee is distinct from v.viewowner
--- SKIP_INTERNAL and table_schema not in ('pg_internal', 'pg_catalog', 'information_schema', 'pg_toast')
--- SKIP_INTERNAL and table_schema not like 'pg_temp_%' and table_schema not like 'pg_toast_temp_%'
-order by schema, name, user;
-
+    acl.grantee != acl.grantor
+    and c.relkind in ('r', 'v', 'm', 'S', 'f', 'p')
+-- SKIP_INTERNAL and nspname not in ('pg_internal', 'pg_catalog', 'information_schema', 'pg_toast')
+-- SKIP_INTERNAL and nspname not like 'pg_temp_%' and nspname not like 'pg_toast_temp_%'
+order by schema, name, "user";
