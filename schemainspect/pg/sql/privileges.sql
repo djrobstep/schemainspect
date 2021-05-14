578a04622edd63997e9524d6faa8bc90a9e9bc6f
mgbiotech <84165050+mgbiotech@users.noreply.github.com>
2021-05-14 12:53:51 +1000

    Include VIEWS in privileges (aka role, permission)
    Currently only table privileges are handled

--- schemainspect/pg/sql/privileges.sql
+++ schemainspect/pg/sql/privileges.sql
@@ -1,16 +1,18 @@
 select
-  table_schema as schema,
-  table_name as name,
-  'table' as object_type,
-  grantee as user,
-  privilege_type as privilege
-from information_schema.role_table_grants
-where grantee != (
-    select tableowner
-    from pg_tables
-    where schemaname = table_schema
-    and tablename = table_name
-)
+    table_schema   as schema,
+    table_name     as name,
+    'table'        as object_type,
+    grantee        as "user",
+    privilege_type as privilege
+from
+    information_schema.role_table_grants r
+    left join pg_tables t
+              on t.schemaname = r.table_schema and t.tablename = r.table_name
+    left join pg_views v
+              on v.schemaname = r.table_schema and v.viewname = r.table_name
+where
+    r.grantee is distinct from t.tableowner
+    and r.grantee is distinct from v.viewowner
 -- SKIP_INTERNAL and table_schema not in ('pg_internal', 'pg_catalog', 'information_schema', 'pg_toast')
 -- SKIP_INTERNAL and table_schema not like 'pg_temp_%' and table_schema not like 'pg_toast_temp_%'
 order by schema, name, user;
