bccde5d643850f2d8e0e3e3fbc274c172532cc77
mgbiotech <84165050+mgbiotech@users.noreply.github.com>
2021-05-14 13:14:40 +1000

    Update privileges.sql

--- schemainspect/pg/sql/privileges.sql
+++ schemainspect/pg/sql/privileges.sql
@@ -7,12 +7,13 @@ select
 from
     information_schema.role_table_grants r
     left join pg_tables t
-              on t.schemaname = r.table_schema and t.tablename = r.table_name
+        on t.schemaname = r.table_schema and t.tablename = r.table_name
     left join pg_views v
-              on v.schemaname = r.table_schema and v.viewname = r.table_name
+        on v.schemaname = r.table_schema and v.viewname = r.table_name
 where
     r.grantee is distinct from t.tableowner
     and r.grantee is distinct from v.viewowner
 -- SKIP_INTERNAL and table_schema not in ('pg_internal', 'pg_catalog', 'information_schema', 'pg_toast')
 -- SKIP_INTERNAL and table_schema not like 'pg_temp_%' and table_schema not like 'pg_toast_temp_%'
 order by schema, name, user;
+
